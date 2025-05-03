a9a2f88bac6c03b89afae3b222546e40ea61923c
Nhannaldo <nguyenhuunhan3354@gmail.com>
2025-05-03 07:40:10 +0700

    feat: add https://exam-document.vercel.app to allowed origins

--- src/server.js
+++ src/server.js
@@ -28,7 +28,12 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(
   cors({
-    origin: ["http://localhost:3000", "http://localhost:3004", "https://exam-document.onrender.com"],
+    origin: [
+      "http://localhost:3000",
+      "http://localhost:3004",
+      "https://exam-document.onrender.com",
+      "https://exam-document.vercel.app",
+    ],
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
